f85d321ac1fdd371f67c7921abf1401b1e77a8a9
Roman Alenskiy <kuperman88@gmail.com>
2019-05-25 16:52:22 +0300

    Updates

---
+++ precache-manifest.b6b96f76ec6b65761d8bf952b9f9d58a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a16bebb466c0518d1f03b6bcee93ed",
+    "url": "/react-kanban/index.html"
+  },
+  {
+    "revision": "a52fc13cf990cef945dc",
+    "url": "/react-kanban/static/css/main.64f654ea.chunk.css"
+  },
+  {
+    "revision": "cd7fb8525fed33367cbb",
+    "url": "/react-kanban/static/js/2.8c81f1aa.chunk.js"
+  },
+  {
+    "revision": "a52fc13cf990cef945dc",
+    "url": "/react-kanban/static/js/main.459bbe24.chunk.js"
+  },
+  {
+    "revision": "2552bfe471c686e65fe7",
+    "url": "/react-kanban/static/js/runtime~main.57763009.js"
+  },
+  {
+    "revision": "0225b3397190eee1c54f1b7fcf7ea579",
+    "url": "/react-kanban/static/media/background.0225b339.jpg"
+  },
+  {
+    "revision": "220fb32e318c5f59fc7bba5c002c00cd",
+    "url": "/react-kanban/static/media/crossIcon.220fb32e.svg"
+  },
+  {
+    "revision": "61af657156eeae5a9839c594b891fda2",
+    "url": "/react-kanban/static/media/plusIcon.61af6571.svg"
+  }
+]);
